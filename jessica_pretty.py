b16aeb3d5df9d35d4d241bf29c17a8ce59a7647f
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-07-13 11:57:51 -0700

    Initial commit

---
+++ jessica_pretty.py
@@ -0,0 +1,13 @@
+"""
+This file says Jessica is amazing
+"""
+
+print("Jessica is my pretty girl")
+while True:
+    answer = input('Do you think Jess is pretty?')
+    if answer == 'yes':
+        print("Good bc ur right")
+        break
+    if answer == "no":
+        print("You're wrong")
+        continue
